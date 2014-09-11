3d0349526d0a8ea905c1c53111ccb89bb74e2a0c
jhermann <jh@web.de>
2014-09-11 17:37:54 +0200

    example for 'theme per node'

--- site.pp
+++ site.pp
@@ -3,13 +3,22 @@
 # via "sudo apply.sh".
 #
 
-node default {
+class themed_devpi_with_nginx ($theme='default') {
     class { 'devpi':
         ensure              => 'latest',
         ensure_supervisor   => 'latest',
-        theme               => 'devpi/themes/default',
+        theme               => "devpi/themes/$theme",
     }
 
     # to disable the NginX proxy, comment this out
     class { 'devpi::nginx': ensure => 'latest', www_default_disable => true }
+}
+
+
+node /lxujhe.*/ {
+    class { 'themed_devpi_with_nginx': theme => 'mam' }
+}
+
+node default {
+    class { 'themed_devpi_with_nginx': }
 }
